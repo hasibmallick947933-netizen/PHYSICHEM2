afeb6921fc4fa0033c892868ee7bb0c16eab0a75
Hasib Mallick <hasibmallick947933@gmail.com>
2026-09-16 18:21:22 +0530

    Delete backend directory

--- backend/utils/generateToken.js
+++
@@ -1,7 +0,0 @@
-import jwt from 'jsonwebtoken'
-
-export default function generateToken(id) {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN || '7d',
-  })
-}
